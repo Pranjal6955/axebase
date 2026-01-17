31cac3f521f5bfe1e6eeeb4d47f0235840581bb7
Pranjal Negi <negipranjal555@gmail.com>
2026-01-17 10:35:06 +0530

    Update src/features/auth/components/register-form.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/features/auth/components/register-form.tsx
+++ src/features/auth/components/register-form.tsx
@@ -30,7 +30,7 @@ const registerSchema = z
   .object({
     fullname: z.string().min(1,"Please enter your full name"),
     email: z.email("Please Enter a valid email address"),
-    password: z.string().min(8, "Password must be atleast 8 characters"),
+    password: z.string().min(8, "Password must be at least 8 characters"),
     confirmpassword: z.string(),
   })
   .refine((data) => data.password === data.confirmpassword, {
